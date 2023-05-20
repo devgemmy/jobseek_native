3170a18bdb68c81d192b606976d1cf0eb096f546
Anthonia Alexis Asikpo <anthonia@nugitech.com>
2023-05-20 21:44:51 +0100

    ...

--- hook/useFetch.js
+++ hook/useFetch.js
@@ -1,7 +1,6 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 
-
 export const useFetch = (endpoint, query) => {
     const [data, setData] = useState([])
     const [isLoading, setIsLoading] = useState(false)
